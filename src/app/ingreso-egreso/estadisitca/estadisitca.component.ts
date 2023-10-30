d9817b61a9581151f3cc38886ac74713a249cbf6
Agustin Osorio <osorioagus96@gmail.com>
2023-10-30 10:56:09 -0300

    clase 63

---
+++ src/app/ingreso-egreso/estadisitca/estadisitca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estadisitca',
+  templateUrl: './estadisitca.component.html',
+  styles: [
+  ]
+})
+export class EstadisitcaComponent {
+
+}
