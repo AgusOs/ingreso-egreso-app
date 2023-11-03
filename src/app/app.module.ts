0f7be962dc9dd500498df51e8d68f81aca19bed5
Agustin Osorio <osorioagus96@gmail.com>
2023-11-03 11:56:05 -0300

    Clase 68

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
-import { EstadisitcaComponent } from './ingreso-egreso/estadisitca/estadisitca.component';
+import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
 import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -20,7 +21,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     RegisterComponent,
     DashboardComponent,
     IngresoEgresoComponent,
-    EstadisitcaComponent,
+    EstadisticaComponent,
     DetalleComponent,
     FooterComponent,
     NavbarComponent,
