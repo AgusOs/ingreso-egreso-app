1f04d8b810db435e5e9d39c5240868e763b6816d
Agustin Osorio <osorioagus96@gmail.com>
2023-11-04 14:11:44 -0300

    Clase 76

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +10,36 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterComponent {
+
+  registroForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, 
+              private AuthService: AuthService,
+              private router: Router){}
+
+  ngOnInit(){
+
+    this.registroForm = this.fb.group({
+      nombre: [ '', Validators.required ],
+      correo: [ '', [Validators.required, Validators.email ]],
+      password: [ '', Validators.required ]
+    })
+  }
+
+  crearUsuario(){
+
+    if( this.registroForm.invalid ){
+      return;
+    }
+
+    const { nombre, correo, password } = this.registroForm.value;
+    this.AuthService.crearUsuario ( nombre, correo, password )
+    .then( credenciales => {
+      console.log( credenciales );
+      this.router.navigate(['/']);
+    })
+    .catch ( error => console.error ( error ) )
+
+  }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ingreso-egreso-app-8218a',
+    appId: '1:99891475331:web:8129008c0afbffb0f57cb6',
+    storageBucket: 'ingreso-egreso-app-8218a.appspot.com',
+    apiKey: 'AIzaSyC9RntR4y5ClWPhq5XbxJZfO7HB3Kh0GzA',
+    authDomain: 'ingreso-egreso-app-8218a.firebaseapp.com',
+    messagingSenderId: '99891475331',
+    measurementId: 'G-77L8LJCYJL',
+  },};
